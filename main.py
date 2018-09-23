fe116b6049a34dbae9d73ec14c296f9c2092ab83
Edvard Krol <freeridik@gmail.com>
2018-09-23 18:04:32 +0300

    added config file

--- main.py
+++ main.py
@@ -2,40 +2,29 @@ import requests_toolbelt.adapters.appengine
 requests_toolbelt.adapters.appengine.monkeypatch()
 from flask import Flask
 import flask_cors
-from firebase_admin import credentials
 import firebase_admin
-import google.auth.transport.requests
+from firebase_admin import credentials
+from firebase_admin import db
+from config import current_config
+
+
 from page import page
 
 
-cred = credentials.Certificate('credentials/serviceAccountKey.json')
+cred = credentials.Certificate(current_config.CREDENTIALS_PATH)
 # Initialize the app with a service account, granting admin privileges
-fire_app = firebase_admin.initialize_app(cred)
+fire_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': current_config.FIREBASE_REALTIME_DB_URL
+})
 
 # Use the App Engine Requests adapter. This makes sure that Requests uses
 # URLFetch.
-HTTP_REQUEST = google.auth.transport.requests.Request()
+
 
 static_dir = '/static'
 
-
-
-# @app.after_request
-# def add_header(r):
-#     """
-#     Add headers to both force latest IE rendering engine or Chrome Frame,
-#     and also to cache the rendered page for 10 minutes.
-#     """
-#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     r.headers["Pragma"] = "no-cache"
-#     r.headers["Expires"] = "0"
-#     r.headers['Cache-Control'] = 'public, max-age=0'
-#     return r
-
 app = Flask(__name__, static_url_path=static_dir)
 flask_cors.CORS(app)
-
-
 
 app.register_blueprint(page)
 
