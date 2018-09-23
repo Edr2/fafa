169eade66b451e1265e5d77341b2a6b5c49e9dfc
Vladyslav <raven73093@gmail.com>
2018-09-23 18:39:40 +0300

    Updated error handling in forms

--- static/script/main.js
+++ static/script/main.js
@@ -1,15 +1,51 @@
 var app = {
-  $sidebar : $('#sidebar'),
-  $userDialog : $('#user-dialog'),
+  dom : {
+    sidebar          : $('#sidebar'),
+    userDialog       : $('#user-dialog'),
+    userProfile      : $('#user-profile-photo'),
+    uploadButton     : $('#upload-button'),
+    uploadMenu       : $('#upload-menu'),
+    userProfilePhoto : $('#user-profile-photo img'),
+  },
 
   toggleSidebar : function() {
-    this.$sidebar.toggleClass('hidden');
+    this.sidebar.toggleClass('hidden');
   },
   toggleUerDialog : function() {
-    this.$userDialog.fadeToggle( 300 );
+    this.userDialog.fadeToggle( 300 );
   },
+
+  init : function() {
+    app.dom.uploadButton.click( this.upload.toggleUploadMenu );
+
+    this.listenAuthChanges();
+  },
+
+  upload : {
+    toggleUploadMenu : function( event ) {
+      app.dom.uploadMenu.slideToggle( 200 );
+
+      console.log('Menu opened');
+    },
+
+  },
+  
+
+  listenAuthChanges : function() {
+    Auth.onAuthStateChanged( function( user ) {
+      if( user ) {
+        let photoUrl = user.providerData[0] ? user.providerData[0].photoURL : user.photoURL;
+        
+        app.dom.userProfilePhoto.attr('src', photoUrl )
+        app.dom.userProfile.removeClass('loading');
+
+        console.log('user : ', user );
+      }
+    });
+  }
 }
 
 $( document ).ready( function()
 {
+  app.init();
 })
