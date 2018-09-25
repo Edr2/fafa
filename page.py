d70c53b9646deb159f990c3899404c51a2160e5a
Edvard Krol <freeridik@gmail.com>
2018-09-25 16:56:32 +0300

    refactoring

--- page.py
+++ page.py
@@ -133,19 +133,6 @@ def logout():
         return redirect('/login')
 
 
-@page.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
-
 @page.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
