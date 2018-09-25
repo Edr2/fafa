f13aa12c36cd6bcc447ffe94c58573a7821f3914
Edvard Krol <freeridik@gmail.com>
2018-09-25 16:56:32 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import requests_toolbelt.adapters.appengine
 requests_toolbelt.adapters.appengine.monkeypatch()
-
 from flask import Flask
 import flask_cors
 import firebase_admin
 from firebase_admin import credentials
 from config import current_config
-
-
 from page import page
-requests_toolbelt.adapters.appengine.monkeypatch()
 
 cred = credentials.Certificate(current_config.CREDENTIALS_PATH)
-# Initialize the app with a service account, granting admin privileges
 fire_app = firebase_admin.initialize_app(cred, {
     'databaseURL': current_config.FIREBASE_REALTIME_DB_URL
 })
-
 
 static_dir = '/static'
 
@@ -24,6 +18,3 @@ app = Flask(__name__, static_url_path=static_dir)
 flask_cors.CORS(app)
 
 app.register_blueprint(page)
-
-# app = create_app()
-# app.run()
